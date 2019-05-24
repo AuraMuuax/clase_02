bf7d2586055f04f1b731376092301c02a1f45415
Estudiante <aura.mariab@gmail.com>
2019-05-23 20:05:57 -0500

    Clase #2

---
+++ src/clase_02.java
@@ -0,0 +1,29 @@
+/** instamciando
+ * programa dde la clase 2 donde se detetalla instancia*/
+public class clase_02 {
+    int num;
+    char letra;
+    double real;
+    float hola;
+    String nombre;
+
+/* metoddo que muestra algo*/
+
+   public void imprimir(){
+        System.out.println("Clase #2);
+        System.out.println("mi nombre -> "+nombre);
+    }
+     int resultado(int edad){
+    int reult = 0
+            reult =
+    }
+    public static void main(String [] arg) {
+        /*estamos instanciando nuestro programa*/
+        int temp;
+     clase_02 n1= new clase_02();{
+     n1.nombre = "Aura Berrocal";
+        n1.imprimir();
+        temp = n1.resultado( 10);
+        System.out.println("El resultado es ->"+temp)
+    }
+}
